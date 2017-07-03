7954f4c07cba1e3140c16abbbc19fa0bd611952e
Hiran <hiran.tassinari@gmail.com>
2017-07-03 14:45:35 -0300

    Adicionado controller e alterações basicas no controller

---
+++ SIFISCON/Infnet - SIFISCON/SIFISCON.MVC/Controllers/ProdutoController.cs
@@ -0,0 +1,170 @@
+﻿using Infnet.SIFISCON.Domain.Entities;
+using Infnet.SIFISCON.Persistence.Context;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace SIFISCON.MVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private SIFISCONContext db = new SIFISCONContext();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            var produtos = db.Produtos.Include(p => p.Fornecedor);
+            return View(produtos.ToList());
+        }
+
+        // GET: Produto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produto/Create
+        public ActionResult Create()
+        {
+            ViewBag.FornecedorId = new SelectList(db.Fornecedor, "Id", "Cnpj");
+            return View();
+        }
+
+        // POST: Produto/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nome,Marca,Estoque,FornecedorId")] Produto produto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Produtos.Add(produto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            ViewBag.FornecedorId = new SelectList(db.Fornecedor, "Id", "Cnpj", produto.FornecedorId);
+            return View(produto);
+        }
+
+        // GET: Produto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FornecedorId = new SelectList(db.Fornecedor, "Id", "Cnpj", produto.FornecedorId);
+            return View(produto);
+        }
+
+        // POST: Produto/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            ViewBag.FornecedorId = new SelectList(db.Fornecedor, "Id", "Cnpj", produto.FornecedorId);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var produto = db.Produtos.Find(id);
+            if (TryUpdateModel(produto, "",
+               new string[] { "Nome,Marca,Estoque,FornecedorId" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+
+            ViewBag.FornecedorId = new SelectList(db.Fornecedor, "Id", "Cnpj", produto.FornecedorId);
+            return View(produto);
+        }
+
+        // GET: Produto/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+
+            Produto produto = db.Produtos.Find(id);
+
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Produto produto = new Produto() { Id = id };
+                db.Entry(produto).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
