781487d16988f0a14720c0d1743a59b1cb9ef3ca
Hiran <hiran.tassinari@gmail.com>
2017-07-08 12:02:31 -0300

    Adicionando regra de negocio do calculo de multa

--- SIFISCON/Infnet - SIFISCON/Infnet.SIFISCON.Domain/Entities/AutoDeInfracao.cs
+++ SIFISCON/Infnet - SIFISCON/Infnet.SIFISCON.Domain/Entities/AutoDeInfracao.cs
@@ -14,8 +14,28 @@ namespace Infnet.SIFISCON.Domain.Entities
 
         public bool Agravante { get; set; }
 
-        public decimal Multa { get; set; }
+        private decimal? Multa { get; set; }
 
         public virtual Processo Processo { get; set; }
+
+        [NotMapped]
+        private static decimal PenaBase => 500m;
+
+        [NotMapped]
+        private static decimal UFIR => 3m;
+
+        public decimal ValorDaMulta()
+        {
+            if (Multa == null || Multa == default(decimal))
+            {
+                var ReceitaBruta = Processo.Fornecedor.ReceitaBruta;
+                var valAgravante = Agravante ? 1m : 0m;
+                var valAtenuante = Atenuante ? 0.33m : 1m;
+
+                Multa = PenaBase + (((ReceitaBruta - 120000m) * 0.1m) + 120000m) * (UFIR * (valAgravante + valAtenuante) * Gravidade);
+            }
+
+            return Multa.Value;
+        }
     }
 }
